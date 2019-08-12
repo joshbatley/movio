7377f18371f20b82d853a1cde3b57ba65732cb51
jbatley <jbatley12@gmail.com>
2019-08-12 17:14:04 +0100

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { h } from 'preact';
 import Layout from 'components/Layout';
-import { BrowserRouter } from 'react-router-dom';
 import SnackbarProvider from 'components/SnackbarProvider';
 
 import Router from './Router';
 
 const App = () => (
-  <BrowserRouter>
     <SnackbarProvider>
       <Layout>
         <Router />
       </Layout>
     </SnackbarProvider>
-  </BrowserRouter>
 );
 
 
